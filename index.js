a8948d248c12655cf5669cba087e03f28a523779
Kolomiiets Lidiia <kolomieclidia7@gmail.com>
2023-06-02 17:48:20 +0300

    complete task number five

---
+++ index.js
@@ -0,0 +1,53 @@
+const numbers = [16,-37,54,-4,72,-56,47,4,-16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47]
+
+let sumPozitifElements = 0;
+let countPozitiveElements = 0;
+let countNegativeElements = 0;
+let evenPozitiveElements = 0;
+let sumEvenPozitiveElements = 0;
+let oddPozitiveElements = 0;
+let sumOddPozitiveElements = 0;
+let productPozitiveElements = 1;
+let maxElement = numbers[0];
+let countMaxElement = 0;
+
+for(let i = 0; i < numbers.length; i++){
+   if(numbers[i] > 0){
+    sumPozitifElements = sumPozitifElements + numbers[i];
+    countPozitiveElements = countPozitiveElements + 1;
+    productPozitiveElements = productPozitiveElements * numbers[i];
+
+   }
+   if (numbers[i] < 0){
+    countNegativeElements = countNegativeElements + 1;
+   }
+
+   if(numbers[i] > 0 && numbers[i] % 2 === 0){
+    evenPozitiveElements = evenPozitiveElements + 1;
+    sumEvenPozitiveElements = sumEvenPozitiveElements + numbers[i];
+
+   }
+   if(numbers[i] > 0 && numbers[i] % 2 !== 0) {
+    oddPozitiveElements = oddPozitiveElements + 1;
+    sumOddPozitiveElements = sumOddPozitiveElements + numbers[i];
+    }
+
+    if(numbers[i] > maxElement){
+        maxElement = numbers [i];
+        countMaxElement = i;
+
+    }
+}
+
+
+let result = "The sum of positive elements is " + sumPozitifElements + ". ";
+let result2 = "\nThe maximum element of the array is " + maxElement + ". ";
+let result3 = "\nIs the sequence number of the maximum element " + countMaxElement + ". ";
+let result4 = "\nThere are a number of positive elements " + countPozitiveElements + ". ";
+let result5 = "\nThere are a number of negative elements " + countNegativeElements + ". ";
+let result6 = "\nNumber of odd positive elements is " + oddPozitiveElements + ". ";
+let result7 = "\nNumber of even positive elements " + evenPozitiveElements + ". ";
+let result8 = "\nThe sum of even positive elements is " + sumEvenPozitiveElements + ". ";
+let result9 = "\nThe sum of odd positive elements is " + sumOddPozitiveElements + ". ";
+let result10 = "\n The product of positive elements is " + productPozitiveElements + ". ";
+    alert(result + result2 + result3 + result4 + result5 + result6 + result7 + result8 + result9 + result10);
